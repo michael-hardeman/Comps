1f017566ff9d00bccd5259366c4aee473ad78492
Michael Hardeman <michael.hardeman@protonmail.com>
2024-05-23 19:06:12 -0400

    Converting mysql

---
+++ mysql/src/mysql.ads
@@ -0,0 +1,3 @@
+package Mysql is
+
+end Mysql;
--- mysql/src/mysql_interface-variable.adb
+++ mysql/src/mysql_interface-variable.adb
@@ -6,7 +6,6 @@ with  -- Standard units
 
 package body Mysql_Interface.Variable is
 --     use Globals;
-
    ---------
    -- Set --
    ---------
@@ -16,17 +15,18 @@ package body Mysql_Interface.Variable is
         "replace into variables values ('" & Variable & "', '" & To & "');";
       Nb_Rows : Natural;
    begin
-      Start_Transaction;
+      --  Start_Transaction;
       Exec_SQL (Request, Nb_Rows);
       if Nb_Rows in 1 .. 2 then   -- 1: added, 2: replaced
-         Commit_Transaction;
+         Commit;
       else
-         Rollback_Transaction;
-         DB_Failure ("Set", "variables", Request, "Incorrect affected rows:" & Natural'Image (Nb_Rows));
+         --  Rollback_Transaction;
+         --  DB_Failure ("Set", "variables", Request, "Incorrect affected rows:" & Natural'Image (Nb_Rows));
+         raise Program_Error;
       end if;
    exception
-      when Occur : others =>
-         DB_Failure ("Set", "variables", Request, Occur);
+      when Occur : others => raise Program_Error;
+      --  DB_Failure ("Set", "variables", Request, Occur);
    end Set;
 
    -----------
@@ -34,16 +34,17 @@ package body Mysql_Interface.Variable is
    -----------
 
    function Value (Variable : String) return String is
-      Request : constant String := "select value from variables where name = '" & Variable & "';";
+      Request : constant String :=
+         "select value from variables where name = '" & Variable & "';";
    begin
-      Start_Transaction;
+      --  Start_Transaction;
       return Result : String := Exec_SQL (Request) do
-         Commit_Transaction;
+         Commit;
       end return;
    exception
-      when Occur : others =>
-         Rollback_Transaction;
-         DB_Failure ("Value", "variables", Request, Occur);
+      when Occur : others => return "";
+         --  Rollback_Transaction;
+         --  DB_Failure ("Value", "variables", Request, Occur);
    end Value;
 
    ---------
@@ -98,7 +99,7 @@ package body Mysql_Interface.Variable is
 
    function  Value (Variable : String) return Ada.Calendar.Time is
    begin
-      return Ada.Calendar.Formatting.Value (Value (Variable)(1..19)); -- 1..19: YYYY-MM-DD hh:mm:ss
+      return Ada.Calendar.Formatting.Value (Value (Variable)(1 .. 19)); -- 1 .. 19: YYYY-MM-DD hh:mm:ss
    end Value;
 
 end Mysql_Interface.Variable;
